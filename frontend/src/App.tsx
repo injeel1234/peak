1e1dcd08b79e57c06ed8144eaa53dfcf4b4dd093
Injeel1 <injeel24@gmail.com>
2026-07-08 10:09:22 +0500

    Create landing page hero section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,32 @@
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <h1>Peak 🚀</h1>
-      <p>My app is working!</p>
-    </div>
+    <main className="app">
+      <nav className="navbar">
+        <h2>Peak 🚀</h2>
+
+        <div className="nav-links">
+          <a href="#">Features</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      </nav>
+
+      <section className="hero">
+        <h1>Optimise Your Energy.</h1>
+
+        <h1>Master Your Day.</h1>
+
+        <p>
+          Peak uses AI to analyse your caffeine intake, sleep,
+          and energy levels so you always know the perfect time
+          to focus, rest, or recharge.
+        </p>
+
+        <button>Track Your Energy</button>
+      </section>
+    </main>
   );
 }
 
